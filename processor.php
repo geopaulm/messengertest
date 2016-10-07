35361e7bd47c033cd415383a01b1f9e65ba382df
Geo Paul <geopaulm@gmail.com>
2016-10-08 00:58:13 +0530

    Added verification for facebook

--- processor.php
+++ processor.php
@@ -1,2 +1,14 @@
 <?php
+	/**
+	 This file is the entry point for the Facebook messenger bot service. 
+	 Route all the calls to different classes as per the pattern.
+	**/
+
+	/** Facebook tests whether the webhook is alive by using some get request.
+	Providin the appropriate response. */
+	if($_GET['hub_challenge']) {
+		//This is not the ideal way to do it.
+		echo $_GET['hub_challenge'];
+		exit(0);
+	}
 ?>
