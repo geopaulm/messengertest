9510411c518983c81cb5895f7ca1d82d3ca5d0b6
Geo Paul <geopaulm@gmail.com>
2016-10-08 17:08:16 +0530

    Added classes to send message to a particular id

--- PageManager.php
+++ PageManager.php
@@ -1,7 +1,7 @@
 <?php 
 
 /** Manages page subscription events like messaging and stuff */
-
+require_once('SendMessageManager.php');
 class PageManager {
 	/** Process all the enties */
 	function processEntries($entries) {
@@ -22,8 +22,18 @@ class PageManager {
 	function processMessage($message) {
 		$senderId = $message->sender->id;
 		$timestamp = $message->timestamp;
-		$text = $message->message->text;
-		error_log("Received $text from $senderId at $timestamp");
+		if (isset($message->message)) {
+			//Process incoming message
+			$text = $message->message->text;
+			error_log("Received $text from $senderId at $timestamp");
+		
+			//Sending the text back
+			$result = SendMessageManager::sendTextMessage($senderId,$text);
+		} else if (isset($message->read)) {
+			//process delivery report
+		} else {
+			error_log("Unknown incoming message type : ".json_encode($message));
+		}
 	}
 }
 ?>
